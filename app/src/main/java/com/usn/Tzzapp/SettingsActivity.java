c58e3fb0847b4a2a03f35b0c490cc8963a025386
Markus Fenes <markusfenes@gmail.com>
2020-02-21 17:46:53 +0100

    Done more testing with settings

--- app/src/main/java/com/usn/Tzzapp/SettingsActivity.java
+++ app/src/main/java/com/usn/Tzzapp/SettingsActivity.java
@@ -2,11 +2,9 @@ package com.usn.Tzzapp;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
 
-import com.usn.Tzzapp.R;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -18,16 +16,7 @@ public class SettingsActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
                 .commit();
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-    }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
+
     }
 }
---
+++ app/src/main/java/com/usn/Tzzapp/SettingsFragment.java
@@ -0,0 +1,49 @@
+package com.usn.Tzzapp;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import static android.content.Context.MODE_PRIVATE;
+
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    public SharedPreferences sharedPreferences;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+
+        ListPreference langPref = findPreference("list_preference_lang");
+
+        PreferenceManager preferenceManager = getPreferenceManager();
+
+
+        if (langPref != null) {
+            Log.e("Langpref", langPref.getValue());
+
+
+            /*preferenceManager.getSharedPreferences().edit().putString("langSelected", "no2");
+            Log.e("Langpref", preferenceManager.getSharedPreferences().getString("langSelected","nnn"));*/
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
