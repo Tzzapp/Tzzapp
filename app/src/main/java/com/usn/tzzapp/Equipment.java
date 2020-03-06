1f76620e77d4567a58bf01b90cb0899d12555b3e
Markus Fenes <markusfenes@gmail.com>
2020-03-06 16:26:27 +0100

    Update Equipment.java
    Added databinding and the recycler list/view to the equipment java class

--- app/src/main/java/com/usn/tzzapp/Equipment.java
+++ app/src/main/java/com/usn/tzzapp/Equipment.java
@@ -1,17 +1,64 @@
 package com.usn.tzzapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+
+import com.usn.tzzapp.databinding.ActivityEquipmentBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Equipment extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    private List<EquipmentItem> list = new ArrayList<>();
+
+    private EquipmentAdapter equipmentAdapter = new EquipmentAdapter(list);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_equipment);
+
+        /**
+         * This code block replaces the standard setContentView with the one from DataBindingUtil.
+         * This makes it able to use android data binding, and makes it take objects,
+         * instead of setting the values manually here.
+         */
+        ActivityEquipmentBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_equipment);
+
+        //binding.executePendingBindings();
+        // https://stackoverflow.com/questions/53043412/android-why-use-executependingbindings-in-recyclerview
+        // 06.03.2020
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+
+
+        for (int i = 0; i < 25; i++){
+            list.add(new EquipmentItem("Item" , list.size()+1));
+        }
+
+
+        // Connects to the recycler view in the layout file
+        recyclerView = binding.recview;
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(equipmentAdapter);
+
+
+
+
     }
 
     @Override
@@ -21,4 +68,6 @@ public class Equipment extends AppCompatActivity {
 
         return true;
     }
+
+
 }
