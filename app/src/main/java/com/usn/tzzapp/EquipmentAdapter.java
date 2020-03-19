395d360cd6c3b089a8e36b215ca0cd1a26c0064a
Markus F <mafen@users.noreply.github.com>
2020-03-19 16:17:44 +0100

    Added a click listener to the adapter This will allow for clicking on  one item and the app will respond with actions you can do
    This is still to be decided what the actions will be

--- app/src/main/java/com/usn/tzzapp/EquipmentAdapter.java
+++ app/src/main/java/com/usn/tzzapp/EquipmentAdapter.java
@@ -1,6 +1,8 @@
 package com.usn.tzzapp;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -10,13 +12,22 @@ import com.usn.tzzapp.databinding.EquipmentItemBinding;
 
 import java.util.List;
 
+/**
+ * This adapter will allow, the list to be shown to the user in the recycler view.
+ *
+ * It will also allow the list to be customised to fit our needs.
+ */
+
 public class EquipmentAdapter extends RecyclerView.Adapter<EquipmentAdapter.EquipmentViewHolder> {
 
     private List<EquipmentItem> equipmentItemList;
+    private OnEquimentListener onEquimentListener;
 
-    public EquipmentAdapter(List<EquipmentItem> list) {
+
+    public EquipmentAdapter(List<EquipmentItem> list, OnEquimentListener onEquimentListener) {
+
         this.equipmentItemList = list;
-
+        this.onEquimentListener = onEquimentListener;
     }
 
     public void setEquipmentItemList(List<EquipmentItem> equipmentItemList) {
@@ -30,7 +41,7 @@ public class EquipmentAdapter extends RecyclerView.Adapter<EquipmentAdapter.Equi
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         EquipmentItemBinding itemBinding = EquipmentItemBinding.inflate(layoutInflater, parent, false);
 
-        return new EquipmentViewHolder(itemBinding);
+        return new EquipmentViewHolder(itemBinding, onEquimentListener);
     }
 
     @Override
@@ -49,13 +60,26 @@ public class EquipmentAdapter extends RecyclerView.Adapter<EquipmentAdapter.Equi
     }
 
 
-    class EquipmentViewHolder extends RecyclerView.ViewHolder {
+    /**
+     * This interface is used to find out what is clicked on the screen/list
+     * And what position it has .
+     */
+    public interface OnEquimentListener{
+        void onEquipmentClick(int pos);
+    }
 
+
+    class EquipmentViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        OnEquimentListener onEquimentListener;
         private EquipmentItemBinding binding;
 
-        public EquipmentViewHolder(EquipmentItemBinding binding) {
+
+        public EquipmentViewHolder(EquipmentItemBinding binding , OnEquimentListener onEquimentListener) {
             super(binding.getRoot());
             this.binding = binding;
+            this.onEquimentListener = onEquimentListener;
+            itemView.setOnClickListener(this);
 
         }
 
@@ -73,5 +97,20 @@ public class EquipmentAdapter extends RecyclerView.Adapter<EquipmentAdapter.Equi
             binding.executePendingBindings();
         }
 
+        /**
+         * This will allow the view holder to find out which item was clicked.
+         * To do this it uses the OnClickListener from Android.
+         *
+         * To be used it has to be implemented in the class,
+         * where the recycler view is used.
+         * @param v
+         *
+         * For it to work, itemView.setOnClickListener(this) has to be set in the View Holder constructor
+         * Item view does not have to be declared, it is declared in Equipment Adapters super class (RecyclerView.java)
+         */
+        @Override
+        public void onClick(View v) {
+            onEquimentListener.onEquipmentClick(getAdapterPosition());
+        }
     }
 }
