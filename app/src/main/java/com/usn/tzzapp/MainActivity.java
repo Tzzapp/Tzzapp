4d912ae11c3102c0b7a54e22738130ae4fdc3997
Markus F <mafen@users.noreply.github.com>
2020-04-20 18:47:20 +0200

    Made change lang do the whole app
    Before it was only doing text boxes now it does titles as well

--- app/src/main/java/com/usn/tzzapp/MainActivity.java
+++ app/src/main/java/com/usn/tzzapp/MainActivity.java
@@ -100,19 +100,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void changeLang(String lang) {
 
-
-
-        Locale locale1 = new Locale(lang);
-
         Resources res = getResources();
 
         Configuration configuration = res.getConfiguration();
 
-        configuration.locale = locale1;
-
+        configuration.setLocale(new Locale(lang));
         DisplayMetrics dm = res.getDisplayMetrics();
 
         res.updateConfiguration(configuration, dm);
+        getResources().updateConfiguration(configuration,dm);
+        getApplicationContext().getResources().updateConfiguration(configuration, dm);
 
         
     }
