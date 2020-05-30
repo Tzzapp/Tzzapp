35e946f93f7e081e26c8715f8e828a6a34b939b7
Mads <madsfredrikhagen@hotmail.com>
2020-05-30 17:00:22 +0200

    early GUI established

--- app/src/main/java/com/usn/tzzapp/Heating.java
+++ app/src/main/java/com/usn/tzzapp/Heating.java
@@ -19,13 +19,6 @@ public class Heating extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 }
