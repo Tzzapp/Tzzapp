c46544e770bdbcd07e488d23934b86292c97af3c
Mads <madsfredrikhagen@hotmail.com>
2020-05-30 19:49:30 +0200

    variables declared and id's found

--- app/src/main/java/com/usn/tzzapp/Heating.java
+++ app/src/main/java/com/usn/tzzapp/Heating.java
@@ -8,9 +8,22 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Editable;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Heating extends AppCompatActivity {
+
+    EditText ariaX;
+    EditText ariaY;
+    EditText ariaTotal;
+    EditText cableLenght;
+    EditText cc; //cc is the profecjonal term for the distance betwen difrent sections of the cable as it bends
+
+    Button MathMaster;
+    Editable holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,28 @@ public class Heating extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ariaX = findViewById(R.id.editTextAriaX);
+        ariaY = findViewById(R.id.editTextAriaY);
+        ariaTotal = findViewById(R.id.editTextAriaTotal);
+        cableLenght = findViewById(R.id.editTextCableLength);
+        cc = findViewById(R.id.editTextCc);
 
+
+        MathMaster = findViewById(R.id.buttonCalculate);
+
+
+
+        MathMaster.setOnClickListener(v -> {
+
+            double aria, cable;
+
+        });
     }
+/*
+    private void disableEditors(){
+        responseX.setKeyListener(null);
+        responseY.setKeyListener(null);
+        responseWallX.setKeyListener(null);
+        responseWallY.setKeyListener(null);
+    }*/
 }
