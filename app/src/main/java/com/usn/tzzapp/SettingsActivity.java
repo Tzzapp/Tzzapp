e14a6848a329fd64c76f3e3a1ce92b2621597dec
Markus F <mafen@users.noreply.github.com>
2020-06-01 18:58:55 +0200

    Revert "Removed code with no function"
    This reverts commit 92888565

--- app/src/main/java/com/usn/tzzapp/SettingsActivity.java
+++ app/src/main/java/com/usn/tzzapp/SettingsActivity.java
@@ -21,6 +21,14 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onNightModeChanged(int mode) {
+        super.onNightModeChanged(mode);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        new LangUtil(getResources(), this).changeLang(sharedPreferences.getString("lang", ""));
+    }
+
     /* When the user presses the back-button on the actionbar,
        the user is taken back to the app's front page(MainActivity)
      */
