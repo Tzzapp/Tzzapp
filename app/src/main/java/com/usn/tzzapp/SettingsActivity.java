04744d567bde2f9e17324240e9dd89cec406ef95
Markus F <mafen@users.noreply.github.com>
2020-06-01 14:59:09 +0200

    Removed an unused import

--- app/src/main/java/com/usn/tzzapp/SettingsActivity.java
+++ app/src/main/java/com/usn/tzzapp/SettingsActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
 
 
 public class SettingsActivity extends AppCompatActivity {
